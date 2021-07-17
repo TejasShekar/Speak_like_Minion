1efa437524ed5e132af68e3de8d94498ffff5ed9
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-07-17 20:38:17 +0530

    Major update v0.1

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var translateBtn = document.querySelector(#translate-btn);
-var inputTxt = document.querySelector(textarea);
-var outputTxt = document.querySelector(.output);
+var inputTxt = document.querySelector("#input-txt")
+var translateBtn = document.querySelector("#translate-btn")
+var outputTxt = document.querySelector("#output")
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function translateURL(input){
+    return serverURL+"?"+"text="+input;
+}
+function clickEventHandler(){
+    // var txtInput = inputTxt.value; 
+    //The above line will grad value from textarea and assign it to a variable 'txtInput'. 
+
+    fetch(translateURL(inputTxt.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedTxt = json.contents.translated;
+        outputTxt.innerHTML = translatedTxt;
+    })
+
+    // The below gives a explanation of the arrow functions used above
+
+    // .then(function responseHandler(response){ return response.json()})
+    // .then(function logJson(json){
+    //     var translatedTxt = json.contents.translated;
+    //     outputTxt.innerHTML = translatedTxt;
+    // })
+}
+
+translateBtn.addEventListener("click", clickEventHandler)
