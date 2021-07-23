5d8cb3db114f975f2319cb28476c0ada63e5f2e3
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-07-23 13:27:08 +0530

    refactor : Clean code using prettier extension

--- js/app.js
+++ js/app.js
@@ -4,31 +4,33 @@ var outputTxt = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function translateURL(input){
-    return serverURL+"?"+"text="+input;
+function translateURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
-function errorHandler(error){
-    console.log("Error occured : ", error);
-    alert("An error has occured. Please check whether you are connected to the internet. If yes, try again after sometime.");
+function errorHandler(error) {
+  console.log("Error occured : ", error);
+  alert(
+    "An error has occured. Please check whether you are connected to the internet. If yes, try again after sometime."
+  );
 }
-function clickEventHandler(){
-    // var txtInput = inputTxt.value; 
-    //The above line will grab value from textarea and assign it to a variable 'txtInput'. 
+function clickEventHandler() {
+  // var txtInput = inputTxt.value;
+  //The above line will grab value from textarea and assign it to a variable 'txtInput'.
 
-    fetch(translateURL(inputTxt.value))
-    .then(response => response.json())
-    .then(json => {
-        var translatedTxt = json.contents.translated;
-        outputTxt.innerHTML = translatedTxt;
+  fetch(translateURL(inputTxt.value))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      outputTxt.innerHTML = translatedTxt;
     })
-    .catch(errorHandler)
+    .catch(errorHandler);
 }
-    // The below gives a explanation of the arrow functions used above
+// The below gives a explanation of the arrow functions used above
 
-    // .then(function responseHandler(response){ return response.json()})
-    // .then(function logJson(json){
-    //     var translatedTxt = json.contents.translated;
-    //     outputTxt.innerHTML = translatedTxt;
-    // })
+// .then(function responseHandler(response){ return response.json()})
+// .then(function logJson(json){
+//     var translatedTxt = json.contents.translated;
+//     outputTxt.innerHTML = translatedTxt;
+// })
 
 translateBtn.addEventListener("click", clickEventHandler);
